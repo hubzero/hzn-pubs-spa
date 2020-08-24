80f63509bd37814e7a7c7f793a62dcf4c743055d
JBG <jbg@peerparty.org>
2020-08-24 13:15:32 +0200

    added some response tests

--- src/test/pubs/res_test.cljs
+++ src/test/pubs/res_test.cljs
@@ -2,6 +2,7 @@
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
             [pubs.handlers.res :as res]
             [pubs.hub-test :refer [resps]]
+            [pubs.utils :as utils]
             )
   )
 
@@ -108,5 +109,97 @@
     )
   )
 
+(deftest prj
+  (let [db (res/prj {} [:res/prj (:prj resps)])]
+    (-> db (get-in [:data :prj]) nil? not is)
+    )
+  )
 
+(deftest new-author
+  (let [auth (:new-author resps)
+        db (res/new-author {} [:res/new-author auth])]
+    (-> db (get-in [:data :authors-list]) nil? not is)
+    (-> db (get-in [:data :authors-list]) count (= 1) is)
+    (-> db (get-in [:data :authors-list (utils/->keyword (:id auth))]) nil? not is)
+    )
+  )
+
+(deftest tags
+  (let [db (res/tags {} [:res/tags (:tags resps)])]
+    (-> db (get-in [:data :tags]) nil? not is)
+    (-> db (get-in [:data :tags]) map? is)
+    (-> db (get-in [:data :tags]) count (= 1) is)
+    (-> db (get-in [:data :tags]) first second :raw_tag (= "core-foo"))
+    )
+  )
+
+(deftest add-tag 
+  (let [db (res/add-tag {:tag-query "foo"
+                         :ui {:tag true}
+                         } [:res/add-tag (:add-tag resps)])]
+    (-> db (get-in [:ui :tag-str]) count (= 0) is)
+    (-> db (get-in [:tag-query]) nil? is)
+    (-> db (get-in [:ui :tag]) not is)
+    )
+  )
+
+(deftest search-tags 
+  (let [db (res/search-tags {} [:res/search-tags (:search-tags resps)])]
+    (-> db :tag-results nil? not is)
+    (-> db :tag-results count (= 2) is)
+    (-> db :tag-results vector? is)
+    )
+  )
+
+(deftest licenses 
+  (let [db (res/licenses {} [:res/licenses (:licenses resps)])]
+    (-> db :licenses nil? not is)
+    (-> db :licenses count (= 2) is)
+    (-> db :licenses vector? is)
+    )
+  )
+
+(deftest license 
+  (let [db (res/license {} [:res/license (:license resps)])]
+    (-> db (get-in [:data :licenses]) nil? not is)
+    (-> db (get-in [:data :licenses]) map? is)
+    (-> db (get-in [:data :licenses :id]) (= 2) is)
+    )
+  )
+
+(deftest save-pub 
+  (let [db (res/save-pub {} [:res/save-pub (:save-pub resps)])]
+    (-> db (get-in [:data :pub-id]) nil? not is)
+    (-> db (get-in [:data :ver-id]) nil? not is)
+    )
+  )
+
+(deftest citations 
+  (let [db (res/citations {} [:res/citations (:citations resps)])]
+    (-> db (get-in [:data :citations]) nil? not is)
+    (-> db (get-in [:data :citations]) map? is)
+    (-> db (get-in [:data :citations]) count (= 1) is)
+    (-> db (get-in [:data :citations]) first first keyword? is)
+    )
+  )
+
+(deftest add-citation
+  (let [c (:add-citation resps)
+        db (res/add-citation {} [:res/add-citation c])]
+    (-> db (get-in [:data :citations]) count (= 1) is)
+    (-> db (get-in [:data :citations (utils/->keyword (:id c))]) nil? not is)
+    )
+  )
+
+(deftest rm-citation
+  (let [c (:rm-citation resps)
+        db (-> {}
+               (res/add-citation [:res/add-citation c])
+               (res/rm-citation [:res/rm-citation c])
+               )
+        ]
+    (-> db (get-in [:data :citations]) count (= 0) is)
+    (-> db (get-in [:data :citations (utils/->keyword (:id c))]) nil? is)
+    )
+  )
 
