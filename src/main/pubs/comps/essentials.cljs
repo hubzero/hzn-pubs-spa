657a32de87aafb792a680d76a8581910da67f538
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-10-16 10:44:45 -0700

    Code formatting, linting

--- src/main/pubs/comps/essentials.cljs
+++ src/main/pubs/comps/essentials.cljs
@@ -1,47 +1,38 @@
 (ns pubs.comps.essentials
   (:require [pubs.comps.agreements :as agreements]
             [pubs.comps.authors-options :as authors-options]
-            [pubs.comps.collection :as collection] 
+            [pubs.comps.collection :as collection]
             [pubs.comps.licenses :as licenses]
             [pubs.comps.master-types :as master-types]
             [pubs.comps.tags :as tags]
-            [pubs.comps.textfield :as textfield] 
-            [pubs.comps.textarea :as textarea]
-            )
-  )
+            [pubs.comps.textfield :as textfield]
+            [pubs.comps.textarea :as textarea]))
 
 (defn authors [s k e]
   (.preventDefault e)
   (.stopPropagation e)
-  (re-frame.core/dispatch [:options/authors])
-  )
+  (re-frame.core/dispatch [:options/authors]))
 
 (defn- content-type [s]
   (case (get-in s [:data :master-type :master-type])
     "Databases" collection/dbs
     "Series" collection/series
-    collection/files 
-    )
-  )
+    collection/files))
 
 (defn- panel-type [s]
   (case (get-in s [:data :master-type :master-type])
     "Databases" :databases
     "Series" :series
-    :content
-    )
-  )
+    :content))
 
 (defn render [s]
   [:fieldset.fieldset-section
    [:header
     [:legend "Essentials"]
-    [:div.note "all fields required"]
-    ]
+    [:div.note "all fields required"]]
    (master-types/render s)
    (textfield/render s "a-title" "Title:" :title)
    (textarea/render s "a-abstract" "Abstract:" :abstract)
-   ;(textarea/render s "a-description" "Description:" :description)
    (collection/render s
                       "a-content"
                       "Content (drag to reorder):"
@@ -54,10 +45,7 @@
                       :authors-list
                       (authors-options/render s)
                       authors)
-
    (tags/render s)
    (licenses/render s)
-   (agreements/render s)
-   ]
-  )
+   (agreements/render s)])
 
