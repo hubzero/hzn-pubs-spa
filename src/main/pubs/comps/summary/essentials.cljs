c6e9f915ee2970e6c4a21f8fa9e10a8d91ba774b
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-10-16 07:42:49 -0700

    Code formatting, linting

--- src/main/pubs/comps/summary/essentials.cljs
+++ src/main/pubs/comps/summary/essentials.cljs
@@ -1,14 +1,11 @@
 (ns pubs.comps.summary.essentials
-  (:require [pubs.comps.summary.section :as section])
-  )
+  (:require [pubs.comps.summary.section :as section]))
 
 (defn- content-type [s]
   (case (get-in s [:data :master-type :master-type])
     "Databases" [[:data :databases] "Databases" :databases false]
     "Series" [[:data :series] "Series" :series false]
-    [[:data :content] "Content" :files false]
-    )
-  )
+    [[:data :content] "Content" :files false]))
 
 (defn render [s]
   (section/render s [[[:data :doi] "DOI" :text true]
@@ -18,7 +15,4 @@
                      [[:data :authors-list] "Authors" :authors-list false]
                      [[:data :tags] "Tags" :tags false]
                      [[:data :licenses] "License" :license false]
-                     [[:terms] "Agreements" :text false]
-                     ]) 
-  )
-
+                     [[:terms] "Agreements" :text false]]))
--- src/main/pubs/comps/summary/section.cljs
+++ src/main/pubs/comps/summary/section.cljs
@@ -2,12 +2,10 @@
   (:require [pubs.comps.summary.field :as field]
             [pubs.comps.summary.collection :as collection]
             [pubs.comps.summary.license :as license]
-            [pubs.comps.summary.tags :as tags]
-            )
-  )
+            [pubs.comps.summary.tags :as tags]))
 
 (defn- fieldtype [s k t bold?]
-  ((t {:text #(field/render s k bold?)               
+  ((t {:text #(field/render s k bold?)
        :files #(collection/render s k t)
        :databases #(collection/render s k t)
        :authors-list #(collection/render s k t)
@@ -15,20 +13,13 @@
        :images #(collection/render s k t)
        :tags #(tags/render s)
        :citations #(collection/render s k t)
-       :series #(collection/render s k t)
-       }))
-  )
+       :series #(collection/render s k t)})))
 
 (defn render [s fields]
   [:section {:class [:fieldset :no-header]}
    (doall (map
             (fn [[k label t bold?]]
               [:div {:class [:field :field-summary] :key label}
-               [:p {:class :label} (str label ":")] 
-               (fieldtype s k t bold?)             
-               ]
-              )
-            fields))
-   ]
-  )
-
+               [:p {:class :label} (str label ":")]
+               (fieldtype s k t bold?)])
+            fields))])
