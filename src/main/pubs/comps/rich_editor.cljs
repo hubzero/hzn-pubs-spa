7d57e0e2d808634e6ed8c5460d60fc0153ae1392
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-10-23 12:28:13 -0700

    Remove rich text editor CDN style loading

--- src/main/pubs/comps/rich_editor.cljs
+++ src/main/pubs/comps/rich_editor.cljs
@@ -5,10 +5,6 @@
   (re-frame.core/dispatch [:text/change k new-value])
   (re-frame.core/dispatch [:req/save-pub]))
 
-(def quill-styling-source
-  {:rel "stylesheet"
-   :href "//cdn.quilljs.com/1.2.6/quill.snow.css"})
-
 (def modules
   {:toolbar [[{"header" [2 3 false]}]
              ["bold" "italic" "underline"]
@@ -20,7 +16,6 @@
 
 (defn render [state id title k]
   [:div.field {:id id :class (if (get-in state [:ui :errors k]) :with-error)}
-   [:link quill-styling-source]
    [:label {:for :title} title]
    [:> ReactQuill
     {:modules modules
