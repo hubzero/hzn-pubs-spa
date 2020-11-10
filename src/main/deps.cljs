88c36bd38f92ba3b4351d5cc87d7e5770b934211
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-11-10 09:29:59 -0800

    Revert npm packages to compatible versions
    fixes:
    - https://purr.purdue.edu/support/ticket/2231
    - https://purr.purdue.edu/support/ticket/2212

--- src/main/deps.cljs
+++ src/main/deps.cljs
@@ -1,10 +1,10 @@
 {:npm-dev-deps {"enzyme" "^3.11.0"
                 "enzyme-adapter-react-16.3" "^1.7.2"
                 "shadow-cljs" "2.9.3"
-                "create-react-class" "^15.6.3",
-                "react" "^16.9.0"
-                "react-dom" "^16.9.0"
-                "react-sortablejs" "^2.0.11"
+                "create-react-class" "15.6.3",
+                "react" "16.13.1"
+                "react-dom" "16.13.1"
+                "react-sortablejs" "2.0.11"
                 "slate" "^0.58.4"
                 "slate-react" "^0.58.4"
                 "slate-history" "^0.58.4"
