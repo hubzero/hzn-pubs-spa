4dcfaef9f2ce6c47d2c43111e8c79e0d679efb9e
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-11-18 12:49:39 -0800

    Add additional npm dependendcy

--- src/main/deps.cljs
+++ src/main/deps.cljs
@@ -3,15 +3,12 @@
                 "shadow-cljs" "2.9.3"
                 "create-react-class" "15.6.3",
                 "react" "16.13.1"
-                "react-dom" "16.13.1"
-                }
- :npm-deps {
-            "slate" "^0.58.4"
+                "react-dom" "16.13.1"}
+ :npm-deps {"slate" "^0.58.4"
             "slate-react" "^0.58.4"
             "slate-history" "^0.58.4"
             "react-sortablejs" "2.0.11"
             "sortablejs" "1.12.0"
             "buffer" "6.0.0"
-            "react-quill" "1.3.5"
-            }}
-
+            "process" "0.11.10"
+            "react-quill" "1.3.5"}}
