3bb7385c47667dd16e7b79f580cc021be96e90b3
Damion Junk <junkda@gmail.com>
2020-12-17 16:31:44 -0600

    Fixes PURR-2246 and PURR-2247, and possibly future similar issues with the files sortable list.
    Only calls API if data has changed, this requires an API fix for complete resolution.

--- project.clj
+++ project.clj
@@ -11,48 +11,49 @@
                  [cljs-http "0.1.46"]
                  [day8.re-frame/test "0.1.5"]
                  ]
-  :plugins [[lein-shadow "0.2.0"]
+  :plugins [[lein-shadow "0.3.1"]
             [lein-shell "0.5.0"]]
   :min-lein-version "2.9.0"
   :source-paths ["src/main" "src/test"]
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
-                                    "/home/jbg/Development/hub/app/components/com_pubs/site/assets/js/compiled"
                                     "target"]
   :shell {:commands {"open" {:windows ["cmd" "/c" "start"]
                              :macosx  "open"
                              :linux   "xdg-open"}}}
-  :shadow-cljs {:nrepl {:port 8777}
-                :builds {:app {:target :browser
-                               :output-dir "resources/public/js/compiled"
-                               :asset-path "/js/compiled"
-                               :modules {:app {:init-fn pubs.core/init}}
-                               :devtools {:http-root "resources/public"
-                                          :http-port 8280
-                                          }
-                               }
-                         :hzn {:target :browser
-                               :output-dir "resources/public/js/compiled"
-                               :asset-path "/app/components/com_pubs/site/assets/js/compiled"
-                               :modules {:app {:init-fn pubs.core/init}}
-                               :compiler-options {:infer-externs :auto}}      
-                         :hub {:target :browser
-                               :output-dir "/home/jbg/Development/hub/app/components/com_pubs/site/assets/js/compiled"
-                               :asset-path "/app/components/com_pubs/site/assets/js/compiled"
-                               :modules {:app {:init-fn pubs.core/init}}
-                               :compiler-options {:infer-externs :auto}
-                               }
-                         :test {:target :node-test
+  :shadow-cljs {:nrepl  {:port 8777}
+                :builds {
+                         :hzn  {:target           :browser
+                                :output-dir       "resources/public/js/compiled"
+                                :asset-path       "/app/components/com_pubs/site/assets/js/compiled"
+                                :modules          {:app {:init-fn pubs.core/init}}
+                                :compiler-options {:infer-externs :auto}}
+
+                         :zapp  {:target           :browser
+                                :output-dir       "resources/public/js/compiled"
+                                :asset-path       "/app/components/com_pubs/site/assets/js/compiled"
+                                :modules          {:app {:init-fn pubs.core/init
+                                                         :entries [devtools.preload
+                                                                   day8.re-frame-10x.preload]
+                                                         }}
+                                :compiler-options {:closure-defines               {re-frame.trace.trace-enabled? true}
+                                                   :silence-optimizations-warning true
+                                                   :optimizations                 :none
+                                                   :infer-externs :auto}
+                                }
+                         :test {:target    :node-test
                                 :output-to "out/node-tests.js"
                                 :autorun   true}
                          }}
   :aliases {"dev"          ["with-profile" "dev" "do"
-                            ["shadow" "watch" "app"]]
+                            ["shadow" "watch" "hzn"]]
+            "10x"          ["with-profile" "dev" "do"
+                            ["shadow" "watch" "zapp"]]
             "hub"          ["with-profile" "prod" "do"
                             ["shadow" "watch" "hub"]]
             "test"         ["with-profile" "dev" "do"
                             ["shadow" "watch" "test"]]
             "prod"         ["with-profile" "prod" "do"
-                            ["shadow" "release" "app"]]
+                            ["shadow" "release" "hzn"]]
             "build-report" ["with-profile" "prod" "do"
                             ["shadow" "run" "shadow.cljs.build-report" "app" "target/build-report.html"]
                             ["shell" "open" "target/build-report.html"]]
@@ -61,8 +62,11 @@
                             ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]}
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "1.0.2"]]
-    :source-paths ["dev"]}
+         {:dependencies [[binaryage/devtools "1.0.2"]
+                         [day8.re-frame/re-frame-10x "0.7.0"]
+                         [day8.re-frame/tracing "0.6.0"]
+                         ]
+          :source-paths ["dev"]}
    :prod {}
    }
   :prep-tasks [])
